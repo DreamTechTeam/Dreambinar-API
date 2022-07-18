0b9988d710f105e7f24182b72d810cefe306f8d8
ikram-maulana <ikram075@ummi.ac.id>
2022-07-18 20:28:52 +0700

    Feat(Plugins): ➕ Add Some Plugins
    - Add Plugins: nodemailer
    - Add content-type builder: ormawa
    - Update content-type builder: formContact

--- config/plugins.js
+++ config/plugins.js
@@ -19,16 +19,34 @@ module.exports = ({ env }) => ({
   },
   upload: {
     config: {
-      provider: 'cloudinary',
+      provider: "cloudinary",
       providerOptions: {
-        cloud_name: env('CLOUDINARY_NAME'),
-        api_key: env('CLOUDINARY_KEY'),
-        api_secret: env('CLOUDINARY_SECRET'),
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
       },
       actionOptions: {
         upload: {},
         uploadStream: {},
         delete: {},
+      },
+    },
+  },
+  email: {
+    config: {
+      provider: "nodemailer",
+      providerOptions: {
+        host: env("SMTP_HOST", "smtp.example.com"),
+        port: env("SMTP_PORT", 587),
+        auth: {
+          user: env("SMTP_USERNAME"),
+          pass: env("SMTP_PASSWORD"),
+        },
+        // ... any custom nodemailer options
+      },
+      settings: {
+        defaultFrom: "dreamtechteam2022@gmail.com",
+        defaultReplyTo: "dreamtechteam2022@gmail.com",
       },
     },
   },
