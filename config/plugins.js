839087c052594c6ffcf2f97d577287e3941922a9
ikram-maulana <ikram075@ummi.ac.id>
2022-07-19 12:13:47 +0700

    Feat(Events): 🧠 Add Events
    - Add events
    - Configure email plugins

--- config/plugins.js
+++ config/plugins.js
@@ -1,80 +1,86 @@
-module.exports = ({ env }) => ({
-  "rest-cache": {
-    config: {
-      provider: {
-        name: "memory",
-        options: {
-          max: 32767,
-          maxAge: 3600,
+module.exports = ({ env }) => {
+  return {
+    "rest-cache": {
+      config: {
+        provider: {
+          name: "memory",
+          options: {
+            max: 32767,
+            maxAge: 3600,
+          },
         },
-      },
-      strategy: {
-        contentTypes: [
-          // list of Content-Types UID to cache
-          "api::developer.developer",
-          "api::feature.feature",
-        ],
-      },
-    },
-  },
-  upload: {
-    config: {
-      provider: "cloudinary",
-      providerOptions: {
-        cloud_name: env("CLOUDINARY_NAME"),
-        api_key: env("CLOUDINARY_KEY"),
-        api_secret: env("CLOUDINARY_SECRET"),
-      },
-      actionOptions: {
-        upload: {},
-        uploadStream: {},
-        delete: {},
-      },
-    },
-  },
-  email: {
-    config: {
-      provider: "nodemailer",
-      providerOptions: {
-        host: env("SMTP_HOST", "smtp.example.com"),
-        port: env("SMTP_PORT", 587),
-        auth: {
-          user: env("SMTP_USERNAME"),
-          pass: env("SMTP_PASSWORD"),
-        },
-        // ... any custom nodemailer options
-      },
-      settings: {
-        defaultFrom: "dreamtechteam2022@gmail.com",
-        defaultReplyTo: "dreamtechteam2022@gmail.com",
-      },
-    },
-  },
-  transformer: {
-    enabled: true,
-    config: {
-      prefix: "/api/",
-      responseTransforms: {
-        removeAttributesKey: true,
-        removeDataKey: true,
-      },
-    },
-  },
-  seo: {
-    enabled: true,
-  },
-  slugify: {
-    enabled: true,
-    config: {
-      contentTypes: {
-        article: {
-          field: "slug",
-          references: "title",
+        strategy: {
+          contentTypes: [
+            // list of Content-Types UID to cache
+            "api::developer.developer",
+            "api::feature.feature",
+          ],
         },
       },
     },
-  },
-  "import-export-entries": {
-    enabled: true,
-  },
-});
+    upload: {
+      config: {
+        provider: "cloudinary",
+        providerOptions: {
+          cloud_name: env("CLOUDINARY_NAME"),
+          api_key: env("CLOUDINARY_KEY"),
+          api_secret: env("CLOUDINARY_SECRET"),
+        },
+        actionOptions: {
+          upload: {},
+          uploadStream: {},
+          delete: {},
+        },
+      },
+    },
+    email: {
+      config: {
+        provider: "nodemailer",
+        providerOptions: {
+          host: env("SMTP_HOST"),
+          port: env("SMTP_PORT"),
+          secure: true,
+          auth: {
+            user: env("SMTP_USER"),
+            pass: env("SMTP_PASSWORD"),
+          },
+          priority: "normal",
+          tls: {
+            rejectUnauthorized: false,
+          },
+        },
+        settings: {
+          defaultFrom: "dreamtechteam2022@gmail.com",
+          defaultReplyTo: "dreamtechteam2022@gmail.com",
+        },
+      },
+    },
+    transformer: {
+      enabled: true,
+      config: {
+        prefix: "/api/",
+        responseTransforms: {
+          removeAttributesKey: true,
+          removeDataKey: true,
+        },
+      },
+    },
+    seo: {
+      enabled: true,
+    },
+    slugify: {
+      enabled: true,
+      config: {
+        contentTypes: {
+          article: {
+            field: "slug",
+            references: "title",
+          },
+        },
+      },
+    },
+    "import-export-entries": {
+      enabled: true,
+    },
+  };
+};
