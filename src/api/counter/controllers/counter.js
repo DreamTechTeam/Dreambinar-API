f40e1d1563aec437caa3f87b6623e125f687d516
ikram-maulana <ikram075@ummi.ac.id>
2022-07-23 09:45:06 +0700

    Feat(Counter): ➖ Delete Counter API
    - Deleted counter API because some issue
    - Delete slug from events

--- src/api/counter/controllers/counter.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- *  counter controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::counter.counter');
